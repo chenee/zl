6bc53670351728a960e0bfacb47df367dc64103f
chenee <chenee543216@gmail.com>
2016-10-13 00:12:37 +0800

    snsapi_base

--- getuserinfo.php
+++ getuserinfo.php
@@ -3,8 +3,10 @@ $appid='wxbc5b302c49712761';
 $appsecret='a0819adf247e075d4c4b9fa9f1c1c7f0';
 
 $redirect_url = urlencode("http://zl.chenee.cn/zl/redirect.php?lastpage=1");
+$scope = snsapi_userinfo;
+$scope = snsapi_base;
 
-$newURL = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_url&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+$newURL = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_url&response_type=code&scope=$scope&state=STATE#wechat_redirect";
 
 header('Location: '.$newURL);
 
