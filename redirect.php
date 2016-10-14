32be60b1e320843744c982ea36435e97b3f432be
chenee <chenee543216@gmail.com>
2016-10-14 21:55:02 +0800

    add wx

--- redirect.php
+++ redirect.php
@@ -41,17 +41,52 @@ if ($access_token) {
         $url2 = "https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid&lang=zh_CN";
 
         $ret = httpGet($url2);
-    echo $ret;
+        //echo $ret;
 
         $res2 = json_decode($ret);
-        echo "<img src=$res2->headimgurl>";
-
+        //echo "<img src=$res2->headimgurl>";
     }
 
 }
 
 $lastpage = $_REQUEST['lastpage'];
 echo "from this page:   $lastpage";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- TemplateBeginEditable name="doctitle" -->
+    <title>注册界面</title>
+    <!-- TemplateEndEditable -->
+    <!-- TemplateBeginEditable name="head" -->
+    <!-- TemplateEndEditable -->
+</head>
+<form action="user_register.php" method="POST">
+    <input type="hidden" name="wx_openid" value=<?php echo $res2->openid ?> />
+    <input type="hidden" name="wx_nickname" value=<?php echo $res2->nickname ?> />
+    <input type="hidden" name="wx_headimgurl" value=<?php echo $res2->headimgurl ?> />
+    <img src="<?php echo $res2->headimgurl ?>">
+    <p>姓名 <input type="text" name="name" /><br/>
+
+    <p>性别 <input type="text" name="sex" /><br/>
+
+    <p>出生年月 <input type="text" name="birthday"/> <br/>
+
+    <p>电话号码 <input type="text" name="cellphone"/> <br/>
+    <p>Email <input type="text" name="email"/> <br/>
+    <p>公司名称 <input type="text" name="company_name"/> <br/>
+    <p>办公地址 <input type="text" name="company_address"/> <br/>
+    <p>工作经历 <input type="text" name="experience"/> <br/>
+    <p>产品简介</p> <input type="text" name="product_info"/> <br/>
+    <p>所缺资源说明</p> <input type="text" name="source_info"/> <br/>
+
+    <input type="submit" value="注册" />&nbsp;
+    <input type="reset" value="重置"/>
+</form>
+<body>
+</body>
+</html>
 
 
 
