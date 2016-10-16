8f79355d359872a4899cba0ec92a126e4e517c56
chenee <chenee543216@gmail.com>
2016-10-16 12:12:40 +0800

    use 叮一叮

--- wxid.php
+++ wxid.php
@@ -1,4 +1,8 @@
 <?php
-$appid=wxbc5b302c49712761;
-$appsecret=a0819adf247e075d4c4b9fa9f1c1c7f0;
+//微信公众号测试平台
+//$appid=wxbc5b302c49712761;
+//$appsecret=a0819adf247e075d4c4b9fa9f1c1c7f0;
+//叮一叮
+$appid=wx9756628d86fa20fc;
+$appsecret=aa4c2500bcfa15901ed6915a5201a15a;
 
