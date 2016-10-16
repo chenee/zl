f9906a3b291d34be5dce235a6c5d3be0e93fde82
chenee <chenee543216@gmail.com>
2016-10-17 02:43:54 +0800

    rm .idea/workspace.xml

--- WxpayAPI/example/jsapi.php
+++ WxpayAPI/example/jsapi.php
@@ -41,7 +41,7 @@ printf_info($order);
 $jsApiParameters = $tools->GetJsApiParameters($order);
 
 //获取共享收货地址js函数参数
-$editAddress = $tools->GetEditAddressParameters();
+//$editAddress = $tools->GetEditAddressParameters();
 
 //③、在支持成功回调通知中处理成功之后的事宜，见 notify.php
 /**
@@ -107,10 +107,11 @@ $editAddress = $tools->GetEditAddressParameters();
 	window.onload = function(){
 		if (typeof WeixinJSBridge == "undefined"){
 		    if( document.addEventListener ){
-		        document.addEventListener('WeixinJSBridgeReady', editAddress, false);
+		        //document.addEventListener('WeixinJSBridgeReady', editAddress, false);
+				document.addEventListener('WeixinJSBridgeReady',  false);
 		    }else if (document.attachEvent){
-		        document.attachEvent('WeixinJSBridgeReady', editAddress); 
-		        document.attachEvent('onWeixinJSBridgeReady', editAddress);
+		        document.attachEvent('WeixinJSBridgeReady');
+		        document.attachEvent('onWeixinJSBridgeReady');
 		    }
 		}else{
 			editAddress();
