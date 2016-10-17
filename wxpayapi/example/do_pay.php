6f8ed555afc1f79b6046a905369f0b964be6b7eb
chenee <chenee543216@gmail.com>
2016-10-17 12:36:36 +0800

    tmp

--- wxpayapi/example/do_pay.php
+++ wxpayapi/example/do_pay.php
@@ -71,7 +71,8 @@ $jsApiParameters = $tools->GetJsApiParameters($order);
     <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/> 
     <title>微信支付样例-支付</title>
-    <script type="text/javascript">
+	<script src="http://libs.baidu.com/jquery/2.1.4/jquery.min.js"></script>
+	<script type="text/javascript">
 	//调用微信JS api 支付
 	function jsApiCall()
 	{
